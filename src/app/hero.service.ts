7fb589d13943a8227fcda7066783f71cc356f164
Matej Kardoš <kardos@winkros.local>
2019-08-15 11:57:20 +0200

    last commit

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,6 +6,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {catchError, map, tap} from 'rxjs/operators';
 import {HEROES} from './mock-heroes';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { temporaryAllocator } from '@angular/compiler/src/render3/view/util';
 
 
 
@@ -42,15 +43,17 @@ export class HeroService {
 
   /**Post: add a new hero to the server */
   addHero(hero: Hero): Observable<Hero> {
-    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(tap((newHero: Hero) => this.log(`added hero w/ id = ${newHero.id}`)),
-    catchError(this.handleError<Hero>('addHero'))
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id = ${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
     );
   }
 
   /**PUT: update the hero on the server */
   updateHero(hero: Hero): Observable<any> {
-    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(tap(_ => this.log(`updated hero id=${hero.id}`)),
-    catchError(this.handleError<any>('updateHero'))
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     );
   }
 
@@ -58,8 +61,21 @@ export class HeroService {
     const id = typeof hero === 'number' ? hero : hero.id;
     const url = `${this.heroesUrl}/${id}`;
 
-    return this.http.delete<Hero>(url,this.httpOptions).pipe(tap(_ => this.log(`deleted hero id=${id}`)),
-    catchError(this.handleError<Hero>('deleteHero'))
+    return this.http.delete<Hero>(url,this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  /*GET hereoes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]>{
+    if(!term.trim()){
+      //if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes',[]))
     );
   }
 
