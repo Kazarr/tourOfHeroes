9df13f8c378561d6ac06ef21971ddaac84301048
Matej Kardoš <kardos@winkros.local>
2019-08-23 13:25:00 +0200

    Working state for hero detail

--- src/app/heroes/state/heroes.actions.ts
+++ src/app/heroes/state/heroes.actions.ts
@@ -7,13 +7,14 @@ export enum HeroActionTypes {
   InitializeHero = '[Heroes] Initialize hero',
   DeleteHero = '[Heroes] DeleteHero',
   SetCurrentHero = '[Heroes] Set Current Hero',
-  SaveHero = '[Heroes] Save hero'
+  SaveHero = '[Heroes] Save hero',
+  UpdateHero = '[Heroes] Update hero'
 }
 
 export class GetHeroes implements Action {
   readonly type = HeroActionTypes.GetHeroes;
 
-  constructor(public payload: Hero[]) {}
+  constructor(public payload: Hero[]) { }
 }
 
 export class InitializeHero implements Action {
@@ -23,22 +24,28 @@ export class InitializeHero implements Action {
 export class DeleteHero implements Action {
   readonly type = HeroActionTypes.DeleteHero;
 
-  constructor(public payload: Hero) {}
+  constructor(public payload: Hero) { }
 }
 
 export class SetCurrentHero implements Action {
   readonly type = HeroActionTypes.SetCurrentHero;
 
-  constructor(public payload: Hero) {}
+  constructor(public payload: Hero) { }
 }
 
 export class SaveHero implements Action {
   readonly type = HeroActionTypes.SaveHero;
-  constructor(public payload: Hero) {}
+  constructor(public payload: Hero) { }
+}
+
+export class UpdateHero implements Action {
+  readonly type = HeroActionTypes.UpdateHero;
+  constructor(public payload: Hero) { }
 }
 
 export type HeroActions = GetHeroes
   | InitializeHero
   | DeleteHero
   | SetCurrentHero
-  | SaveHero;
+  | SaveHero
+  | UpdateHero;
