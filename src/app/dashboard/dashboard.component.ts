a0198b8cac0aacd89edc39f03ff1a489a04ae0ae
Matej Kardoš <kardos@winkros.local>
2019-08-23 13:02:46 +0200

    Working state for dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,23 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { State } from '../state/app.state';
+import * as fromHeroes from '../heroes/state/heroes.selector';
+import * as heroActions from '../heroes/state/heroes.actions';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: [ './dashboard.component.css' ]
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService,
+              private store: Store<State>) { }
 
   ngOnInit() {
-    this.getHeroes();
+    this.store.pipe(select(fromHeroes.getHeroes)).subscribe(
+      heroes => this.heroes = heroes.slice(1, 5)
+    );
+
+    if (Boolean(this.heroes)) {
+      this.getHeroes();
+    }
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+  }
+
+  onClick(hero: Hero) {
+    this.store.dispatch(new heroActions.SetCurrentHero(hero));
   }
 }
